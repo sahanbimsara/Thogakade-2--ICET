f2d59b830bed7a58ac6245d72a75c9a0eaf940c4
sahanbimsara <sahanbimsara2060@gmail.com>
2025-05-04 14:55:13 +0530

    first commit

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colId;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colSalary;
+
+    @FXML
+    private TableView tblCustomers;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
